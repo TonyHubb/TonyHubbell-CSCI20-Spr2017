abb32f969e36e48e501ce5ebe57d6a066da123ac
Tony Hubbell <thubbell001@student.butte.edu>
2017-01-31 22:22:55 +0000

    lab1.2 commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,19 @@
+//Created By: Tony Hubbell
+ //Created On: 1/31/2017
+ // My ASC|| art project, starting off with a cat
+ 
+ #include <iostream> 
+ using namespace std; 
+ 
+ int main() 
+ { 
+     cout<<"            _____" << endl;
+     cout <<"           (meow!)" << endl;
+     cout <<"   |l___/|  )/" << endl;
+     cout <<"   (_ ^-^)" << endl;
+     cout <<" _  )   (" << endl;
+     cout <<"(( /     l   ( ( (" << endl;
+     cout <<" (  )|| ||    ) ) )" << endl;
+     cout <<" '--''-''-'   >+++>" << endl;
+     
+ }
